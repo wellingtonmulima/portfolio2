75f077e964ebf79ab53a46e28084093600d6110e
Wellington Mulima <159946922+wellingtonmulima@users.noreply.github.com>
2025-02-12 19:18:09 +0300

    modified

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.getElementById("contact-form").addEventListener("submit",function(e){
+    e.preventDefault();
+
+    alert("Thank you for reaching out will get back to you soon");
+    this.reset();
+});
