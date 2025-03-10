e5cb6840d03ff327383818cb1dd377e20b97494d
Wellington Mulima <159946922+wellingtonmulima@users.noreply.github.com>
2025-03-10 19:41:51 +0300

    midified

--- script.js
+++ script.js
@@ -5,10 +5,3 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-
-document.getElementById("contact-form").addEventListener("submit",function(e){
-    e.preventDefault();
-
-    alert("Thank you for reaching out will get back to you soon");
-    this.reset();
-});
